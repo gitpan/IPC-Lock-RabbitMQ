5ab6828eed9449b051736a8ab711d3020b5d9950
Tomas Doran <bobtfish@bobtfish.net>
2012-03-11 10:51:21 +0000

    Import of BOBTFISH/IPC-Lock-RabbitMQ-0.005 from CPAN.
    gitpan-cpan-distribution: IPC-Lock-RabbitMQ
    gitpan-cpan-version:      0.005
    gitpan-cpan-path:         BOBTFISH/IPC-Lock-RabbitMQ-0.005.tar.gz
    gitpan-cpan-author:       BOBTFISH
    gitpan-cpan-maturity:     released

---
+++ lib/IPC/Lock/RabbitMQ/HasTimeout.pm
@@ -0,0 +1,40 @@
+package IPC::Lock::RabbitMQ::HasTimeout;
+use Moose::Role;
+use MooseX::Types::Moose qw/ Int /;
+use AnyEvent;
+use namespace::autoclean;
+
+has timeout => (
+    is => 'ro',
+    isa => Int,
+    default => 30,
+);
+
+sub _gen_timer {
+    my ($self, $cv, $name) = @_;
+    return unless $self->timeout;
+    AnyEvent->now_update;
+    AnyEvent->timer(after => $self->timeout, cb => sub { $cv->croak("$name  timed out after " . $self->timeout) });
+}
+
+1;
+
+=head1 NAME
+
+IPC::Lock::RabbitMQ::HasTimeout - Role for things which timeout.
+
+=head1 ATTRIBUTES
+
+=head2 timeout
+
+The timeout value, in secions.
+
+=head1 METHODS
+
+=head2 _gen_timer
+
+Genertes an AnyEvent->timer for the timeout.
+
+=cut
+
+
